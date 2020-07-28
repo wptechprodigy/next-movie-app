a6e33646e3beedf3e5575792806f493c261f2854
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-07-28 18:19:51 +0100

    integrate carousel into app

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Navbar from '../components/navbar';
 import SideMenu from '../components/sideMenu';
+import Carousel from '../components/carousel';
 
 const Home = () => (
   <div>
@@ -39,74 +40,7 @@ const Home = () => (
         </div>
 
         <div className="col-lg-9">
-          <div
-            id="carouselExampleIndicators"
-            className="carousel slide my-4"
-            data-ride="carousel"
-          >
-            <ol className="carousel-indicators">
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="0"
-                className="active"
-              ></li>
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="1"
-              ></li>
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="2"
-              ></li>
-            </ol>
-            <div className="carousel-inner" role="listbox">
-              <div className="carousel-item active">
-                <img
-                  className="d-block img-fluid"
-                  src="http://placehold.it/900x350"
-                  alt="First slide"
-                />
-              </div>
-              <div className="carousel-item">
-                <img
-                  className="d-block img-fluid"
-                  src="http://placehold.it/900x350"
-                  alt="Second slide"
-                />
-              </div>
-              <div className="carousel-item">
-                <img
-                  className="d-block img-fluid"
-                  src="http://placehold.it/900x350"
-                  alt="Third slide"
-                />
-              </div>
-            </div>
-            <a
-              className="carousel-control-prev"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="sr-only">Previous</span>
-            </a>
-            <a
-              className="carousel-control-next"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="sr-only">Next</span>
-            </a>
-          </div>
+          <Carousel />
 
           <div className="row">
             <div className="col-lg-4 col-md-6 mb-4">

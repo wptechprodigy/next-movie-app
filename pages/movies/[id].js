1c7f71ee9145628be964c900fae06f05e08b1c3e
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-08-04 22:35:21 +0100

    remove unused code

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,10 +1,6 @@
-import { useRouter } from 'next/router';
 import { getMovieById } from '../../actions';
 
 const Movie = ({ movie }) => {
-  const router = useRouter();
-  const { id } = router.query;
-
   return (
     <div className="container">
       <div className="jumbotron">

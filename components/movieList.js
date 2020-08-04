aaf37025af4a8cca1288649680c3c48924608124
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-08-04 21:44:36 +0100

    handle page refresh

--- components/movieList.js
+++ components/movieList.js
@@ -16,7 +16,7 @@ class MovieList extends Component {
         {movies.map((movie) => (
           <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
             <div className="card h-100">
-              <Link href={`/movies/${movie.id}`}>
+              <Link href="/movies/[id]" as={`/movies/${movie.id}`}>
                 <a>
                   <img
                     className="card-img-top"
@@ -27,7 +27,7 @@ class MovieList extends Component {
               </Link>
               <div className="card-body">
                 <h4 className="card-title">
-                  <Link href={`/movies/${movie.id}`}>
+                  <Link href="/movies/[id]" as={`/movies/${movie.id}`}>
                     <a>{movie.name}</a>
                   </Link>
                 </h4>

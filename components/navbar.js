57fdd011eb25ee302b0def03cab232c8a962a682
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-08-04 15:37:53 +0100

    integrate pages into nav component

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div className="container">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand">
           Start Bootstrap
         </a>
         <button
@@ -20,27 +21,35 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home
-                <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Services
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Contact
-              </a>
-            </li>
+            <Link href="/">
+              <li className="nav-item active">
+                <a className="nav-link">
+                  Home
+                  <span className="sr-only">(current)</span>
+                </a>
+              </li>
+            </Link>
+            <Link href="/about">
+              <li className="nav-item">
+                <a className="nav-link">
+                  About
+                </a>
+              </li>
+            </Link>
+            <Link href="/services">
+              <li className="nav-item">
+                <a className="nav-link">
+                  Services
+                </a>
+              </li>
+            </Link>
+            <Link href="/contact">
+              <li className="nav-item">
+                <a className="nav-link">
+                  Contact
+                </a>
+              </li>
+            </Link>
           </ul>
         </div>
       </div>

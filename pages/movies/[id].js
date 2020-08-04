b40cf1d54cab8c14180a212e9e3cf3136d9a7ff5
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-08-04 19:04:45 +0100

    add more structure for individual movie

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -6,7 +6,16 @@ const Movie = () => {
 
   return (
     <div className="container">
-      <h1>Movie with id: {id}</h1>
+      <div className="jumbotron">
+        <h1 className="display-4">Movie Title</h1>
+        <p className="lead">This is a simple hero unit.</p>
+        <hr className="my-4" />
+        <p>It uses some blah blah blah</p>
+        <a className="btn btn-primary btn-lg" href="/" role="button">
+          Learn more
+        </a>
+      </div>
+      <p>Some movie description will go here as well.</p>
     </div>
   );
 };

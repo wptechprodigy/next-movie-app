ab8ddd6e76df66445223bfea09a087e5da27f3d1
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-08-04 18:20:06 +0100

    break index into smaller modules

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
-import Head from 'next/head';
-import Navbar from '../components/navbar';
 import SideMenu from '../components/sideMenu';
 import Carousel from '../components/carousel';
 import MovieList from '../components/movieList';
-import Footer from '../components/footer';
 
 import { getMovies } from '../actions';
 
 const Home = ({ movies }) => {
   return (
-    <div>
-      <Head>
-        <title>Home</title>
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossOrigin="anonymous"
-        />
-        <script
-          src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-          crossOrigin="anonymous"
-        ></script>
-        <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-          integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-          crossOrigin="anonymous"
-        ></script>
-        <script
-          src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-          integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
-
-      <Navbar />
-
-      <div className="container home-page">
+    <>
+      <div className="container">
         <div className="row">
           <div className="col-lg-3">
             <SideMenu name="Movie DB" />
@@ -49,21 +19,12 @@ const Home = ({ movies }) => {
             <Carousel />
 
             <div className="row">
-              <MovieList movies={movies} />
+              <MovieList movies={movies || []} />
             </div>
           </div>
         </div>
       </div>
-      <Footer />
-
-      <style jsx>
-        {`
-          .home-page {
-            padding: 80px;
-          }
-        `}
-      </style>
-    </div>
+    </>
   );
 };
 
@@ -71,8 +32,8 @@ Home.getInitialProps = async () => {
   const movies = await getMovies();
 
   return {
-    movies
-  }
-}
+    movies,
+  };
+};
 
 export default Home;

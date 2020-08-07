66c7b06a547fdbdec8b9add35eabc060ba3d2dc0
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-08-07 08:23:15 +0100

    add cover image to each movie doc

--- actions/index.js
+++ actions/index.js
@@ -11,6 +11,8 @@ const MOVIE_DATA = [
     genre: 'drama',
     image:
       'https://m.media-amazon.com/images/M/MV5BNjQ2NDA3MDcxMF5BMl5BanBnXkFtZTgwMjE5NTU0NzE@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg',
+    coverImage:
+      'https://images.unsplash.com/photo-1567175220991-788d36f3c17a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
   },
   {
     id: '2',
@@ -24,6 +26,8 @@ const MOVIE_DATA = [
     genre: 'action, crime, drama',
     image:
       'https://img.cinemablend.com/filter:scale/quill/c/3/8/0/f/4/c380f4f12cfeec19f0c40c6f57db188f2f98cca8.jpg?mw=600',
+    coverImage:
+      'https://images.unsplash.com/photo-1509347528160-9a9e33742cdb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
   },
   {
     id: '3',
@@ -37,6 +41,8 @@ const MOVIE_DATA = [
     genre: 'adventure, drama, fantasy',
     image:
       'https://img.cinemablend.com/filter:scale/quill/0/f/5/2/a/6/0f52a6843a25c1a5c1f9a0c00548cad9e1d912e2.jpg?mw=600',
+    coverImage:
+      'https://images.unsplash.com/photo-1506466010722-395aa2bef877?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1188&q=80',
   },
 ];
 
